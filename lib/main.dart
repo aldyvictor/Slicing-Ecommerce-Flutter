f46e785bdadac3c0b5fe6b5152b04304d8f7cf8b
Aldy Victor Rianes <aldyvictor1046@gmail.com>
2022-01-26 13:59:38 +0700

    Make Splash Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:shamo/theme.dart';
+import 'package:shamo/pages/sign_in.dart';
+import 'package:shamo/pages/splash_page.dart';
+// import 'package:shamo/theme.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,27 +10,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          'Hallo Semuanya!',
-          style: secondaryTextStyle.copyWith(
-            fontSize: 30,
-          ),
-        ),
-      ),
+      routes: {
+        '/': (context) => const SplashPage(),
+        '/sign-in': (context) => const SignIn(),
+      },
     );
   }
 }
