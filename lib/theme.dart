9b9b745d3db69f6bb0600c0dc7fd8ca1abfea1e9
Aldy Victor Rianes <aldyvictor1046@gmail.com>
2022-01-26 22:21:30 +0700

    Slicing Sign Up Page

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+double defaultMargin = 30.0;
+
 const Color primaryColor = Color(0xff6C5ECF);
 const Color secondaryColor = Color(0xff38ABBE);
 const Color alertColor = Color(0xffED6363);
@@ -10,6 +12,7 @@ const Color backgroundColor2 = Color(0xff2B2937);
 const Color backgroundColor3 = Color(0xff242231);
 const Color primaryTextColor = Color(0xffF1F0F2);
 const Color secondaryTextColor = Color(0xff999999);
+const Color subtitleTextColor = Color(0xff504F5E);
 
 TextStyle primaryTextStyle = GoogleFonts.poppins(
   color: primaryTextColor,
@@ -21,6 +24,14 @@ TextStyle secondaryTextStyle = GoogleFonts.poppins(
 
 TextStyle priceTextStyle = GoogleFonts.poppins(
   color: priceColor,
+);
+
+TextStyle subtitleTextStyle = GoogleFonts.poppins(
+  color: subtitleTextColor,
+);
+
+TextStyle purpleTextStyle  = GoogleFonts.poppins(
+  color: primaryColor,
 );
 
 FontWeight light = FontWeight.w300;
